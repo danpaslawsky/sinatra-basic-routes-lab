e81b5178e81a10c01fa80f7fb670d618bee4f29f
Dan Paslawsky <danpaslawsky@gmail.com>
2022-03-29 17:12:38 -0700

    Done.

--- app.rb
+++ app.rb
@@ -1,5 +1,16 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+    get '/name' do
+        "My name is Dan"
+    end  
     
+    get '/hometown' do
+        "My hometown is San Diego"
+    end
+    
+    get '/favorite-song' do
+        "My favorite song is by the struts"
+    end    
+
 end
